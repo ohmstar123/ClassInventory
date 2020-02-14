f4dde78060c6f9c3fcdc4991bde151cb5353cd34
Ohm Patel <ohmpate114@ed.amdsb.ca>
2020-02-14 13:32:43 -0500

    Done with the first box worth of stuff

--- ClassInventory/Form1.cs
+++ ClassInventory/Form1.cs
@@ -13,6 +13,7 @@ namespace ClassInventory
     public partial class Form1 : Form
     {
         // TODO - create a List to store all inventory objects
+        List<NewPlayer> data = new List<NewPlayer>();
 
         public Form1()
         {
@@ -21,13 +22,22 @@ namespace ClassInventory
 
         private void addButton_Click(object sender, EventArgs e)
         {
+            string name, age, team, position;
             // TODO - gather all information from screen 
+            name = c.Text;
+            age = ageInput.Text;
+            team = teamInput.Text;
+            position = positionInput.Text;
 
             // TODO - create object with gathered information
+            NewPlayer t = new NewPlayer(name, age, team, position);
 
             // TODO - add object to global list
+            data.Add(t);
+            label1.Text = "";
 
             // TODO - display message to indicate addition made
+            label1.Text = "Finished";
         }
 
         private void removeButton_Click(object sender, EventArgs e)
@@ -58,6 +68,12 @@ namespace ClassInventory
             //---------------------------
 
             // TODO - show all objects in list
+            
+
+            foreach (NewPlayer informationList in data)
+            {
+                label1.Text += informationList.name + " " + informationList.age + " " + informationList.team + " " + informationList.position + "\n";
+            }
         }
     }
 }
---
+++ ClassInventory/NewPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassInventory
+{
+    class NewPlayer
+    {
+        public string name, age, team, position;
+        public NewPlayer(string _name, string _age, string _team, string _position)
+        {
+            name = _name;
+            age = _age;
+            team = _team;
+            position = _position;
+
+        }
+    }
+}
